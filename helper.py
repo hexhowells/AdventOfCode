ecda60213eb7186559cdd7381cf404e655056192
Morgan <58524460+hexhowells@users.noreply.github.com>
2022-11-04 23:02:50 +0000

    Add grid class
    makes working with grids easier

--- helper.py
+++ helper.py
@@ -27,6 +27,52 @@ def create_grid(x):
 	return [[int(a) for a in list(line)] for line in x]
 
 
+class Grid:
+	def __init__(self, x, cell_type=int):
+		self.grid = [[cell_type(a) for a in list(line)] for line in x]
+		self.height = len(self.grid)
+		self.width = len(self.grid[0])
+
+
+	def __getitem__(self, x):
+		return self.grid[x]
+
+
+	def __iter__(self):
+		for r in range(self.height):
+			for c in range(self.width):
+				yield self.grid[r][c]
+
+
+	def get_neighbour_coords(self, point, diag=False):
+		(r, c) = point
+		neighbour_cells = [(r-1, c), (r+1, c), (r, c-1), (r, c+1)]
+
+		if diag:
+			neighbour_cells.append([(r-1, c-1), (r-1, c+1), (r+1, c-1), (r+1, c+1)])
+
+		n = []
+		for (r, c) in neighbour_cells:
+			if (0 <= r < self.height) and (0 <= c < self.width):
+				n.append((r, c))
+
+		return n
+
+
+	def get_neighbours(self, point, diag=False):
+		cells = []
+		for (r, c) in self.get_neighbour_coords(point, diag):
+			cells.append(self.grid[r][c])
+
+		return cells
+
+
+	def all_points(self):
+		for r in range(self.height):
+			for c in range(self.height):
+				yield (r, c)
+
+
 def triangle(n):
 	return n * (n + 1) // 2
 
