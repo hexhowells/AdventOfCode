a8707ad2f4b2625866879f81665fb4a1b8d50e47
Morgan <58524460+hexhowells@users.noreply.github.com>
2022-12-08 09:43:48 +0000

    clean up data parsing

--- template.py
+++ template.py
@@ -25,9 +25,7 @@ def part2(x):
 data = collect_input("input.txt")
 #data = collect_input("test_input.txt")
 
-#data = [x for x in data.split('\n\n')]
-data = [x for x in data.split('\n')]
-#data = [int(x) for x in data.split('\n')]
+data = list(map(str, data.split('\n')))
 
 start = timer()
 
