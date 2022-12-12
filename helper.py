e2035e8711efdcb0e97c8acd1d665c97dd53320d
Morgan <58524460+hexhowells@users.noreply.github.com>
2022-12-12 12:04:36 +0000

    Remove old grid functions

--- helper.py
+++ helper.py
@@ -1,32 +1,6 @@
 import re
 
 
-def get_neighbours(grid, point, diag=False):
-	height, width = len(grid), len(grid[0])
-	(r, c) = point
-	neighbour_cells = [(r-1, c), (r+1, c), (r, c-1), (r, c+1)]
-
-	if diag:
-		neighbour_cells.append([(r-1, c-1), (r-1, c+1), (r+1, c-1), (r+1, c+1)])
-
-	n = []
-	for (r, c) in neighbour_cells:
-		if (0 <= r < height) and (0 <= c < width):
-			n.append((r, c))
-
-	return n
-
-
-def all_points(grid):
-	for r in range(len(grid)):
-		for c in range(len(grid[0])):
-			yield (r, c)
-
-
-def create_grid(x):
-	return [[int(a) for a in list(line)] for line in x]
-
-
 class Grid:
 	def __init__(self, x, cell_type=None):
 		if not cell_type: cell_type = type(x[0][0])
