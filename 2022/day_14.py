443134e69577bd9af8fef3ece036aabb5d7b72ef
Morgan <58524460+hexhowells@users.noreply.github.com>
2022-12-14 09:29:57 +0000

    optimise code
    path of grain is stored, once stopped the next grain starts in the previous position of the previous grain

--- 2022/day_14.py
+++ 2022/day_14.py
@@ -42,24 +42,24 @@ def build_walls(x):
 
 def simulate(x, part1):
 	wall, floor = build_walls(x)
-	grain = start = (0, 500)
+	start = (0, 500)
 	sand = set()
+	path = [start]
 
 	while True:
-		grain_moved = False
+		grain_stopped = True
+		grain = path[-1]
 
 		# try to move sand grain
 		for acc in [[1, 0], [1, -1], [1, 1]]:
 			new_grain = (grain[0] + acc[0], grain[1] + acc[1])
 			if (new_grain not in sand) and (not in_wall(new_grain, wall, floor+2)):
-				grain = new_grain
-				grain_moved = True
+				path.append(new_grain)
+				grain_stopped = False
 				break
 
-		if not grain_moved:
-			sand.add(grain)
-			grain = start
-
+		if grain_stopped: sand.add(path.pop())
+	
 		# check to terminate simulation
 		if part1 and grain[0] >= floor: break
 		if not part1 and start in sand: break
