14a148673da403a9a7544a6d77d8aa2d7c0f00d3
Morgan <58524460+hexhowells@users.noreply.github.com>
2023-11-28 18:46:38 +0000

    add string input type check

--- template.py
+++ template.py
@@ -30,12 +30,12 @@ data = list(map(str, data.split('\n')))
 start = timer()
 
 # Part 1
-ans1 = part1(data.copy())
+ans1 = part1(data if type(data) == str else data.copy())
 print(ans1)
 if ans1 != "Part 1 Empty": pyperclip.copy(ans1)
 
 # Part 2
-ans2 = part2(data.copy())
+ans2 = part2(data if type(data) == str else data.copy())
 print(ans2)
 if ans2 != "Part 2 Empty": pyperclip.copy(ans2)
 
