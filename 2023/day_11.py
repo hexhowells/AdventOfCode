100d84b8f65d55b6a183a19fa800ef01c53cde7f
Morgan <58524460+hexhowells@users.noreply.github.com>
2023-12-11 06:05:17 +0000

    improve solution for day 11

--- 2023/day_11.py
+++ 2023/day_11.py
@@ -28,25 +28,11 @@ def expand(galaxies, expand_factor):
 	return new_galaxies
 
 
-def part1(x):
+def solve(x, expand_factor):
 	grid = aoc.Grid(x)
 	galaxies = grid.find('#')
 
-	galaxies = expand(galaxies, 1)
-
-	ans = 0
-	for i in range(len(galaxies)):
-		for j in range(i, len(galaxies)):
-			ans += abs(galaxies[i][0] - galaxies[j][0]) + abs(galaxies[i][1] - galaxies[j][1])
-
-	return ans
-
-
-def part2(x):
-	grid = aoc.Grid(x)
-	galaxies = grid.find('#')
-
-	galaxies = expand(galaxies, 999_999)
+	galaxies = expand(galaxies, expand_factor)
 
 	ans = 0
 	for i in range(len(galaxies)):
@@ -64,12 +50,12 @@ data = list(map(str, data.split('\n')))
 start = timer()
 
 # Part 1
-ans1 = part1(data if type(data) == str else data.copy())
+ans1 = solve(data if type(data) == str else data.copy(), 1)
 print(ans1)
 if ans1 != "Part 1 Empty": pyperclip.copy(ans1)
 
 # Part 2
-ans2 = part2(data if type(data) == str else data.copy())
+ans2 = solve(data if type(data) == str else data.copy(), 999_999)
 print(ans2)
 if ans2 != "Part 2 Empty": pyperclip.copy(ans2)
 
